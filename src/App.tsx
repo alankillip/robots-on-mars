3f78c48d203ea7e647b374717de05406a7a9bdd6
alankillip <alan.kilip@dentsuaegis.com>
2021-10-08 10:58:40 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        '& > :not(style)': {
+        display: "flex",
+        justifyContent: "center",
+        "& > :not(style)": {
           m: 1,
           padding: 10,
-          width: '100%',
-          height: '100%',
+          width: "100%",
+          height: "100%",
         },
       }}
-
     >
       <Paper elevation={3} />
     </Box>
